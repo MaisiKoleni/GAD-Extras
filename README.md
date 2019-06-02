ce18eded609152d587518c54c292b13f6e7494bb
MaisiKoleni <maisi.koleni@t-online.de>
2019-06-02 19:19:22 +0200

    Added Blatt04 Tests and Note

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Entfernt die Tests/Testklassen bitte trotzdem vor der Abgabe, weil sonst die Tes
 Testfälle zu veröffentlichen ist immer ein Risiko, da diese dazu führen können, dass Leute ihren Code verschlimmbessern, wenn die Tests fehlerhaft sind. Auch die Suche nach Fehlern kann frustrierend sein, unter anderem auch weil nicht alle möglichen Fehler von den Testklassen ausreichend behandelt werden. Doch das ist nichts, was sich nicht beheben ließe. Stetig werden die Tests verbessert und aktualisiert, Bugs werden gefixt.
 
 Das größte Problem:<br>
-**Viele arbeiten immer noch lange mit den alten Tests weiter, ohne zu wissen, dass diese überarbeitet wurden.** 
+**Viele arbeiten immer noch lange mit den alten Tests weiter, ohne zu wissen, dass diese überarbeitet wurden.**
 
 Um dieses Problem zu lösen, gibt es jetzt die (von mir sehr empfohlene) Möglichkeit, durch das Hinzufügen von nur einer weiteren Klasse namens [CFUpdate.java](version/CFUpdate.java) automatisch auf neue Versionen zu prüfen. Dies wird parallel zum Test gemacht und fällt kaum auf (außer es gibt eine neue Version). Die Überprüfung findet auch nur *maximal* alle 30 Minuten statt.
 
@@ -24,25 +24,72 @@ Für dieses Opt-In:
 
 _(das geht natürlich erst bei Tests ab Version 1.2 für `blatt02` und Version 1.6 für `blatt03`; bei allen anderen Blättern immer)_
 
-## [Blatt02](blatt02/)
-Testklasse für das Testen von higher/lower bei BinSea. Ausführbar über die main-Methode. Lässt sich einfach zu den anderen Klassen packen.
+## [Blatt02](blatt02/) <span style="color:grey;font-size:large"> // Version 1.2</span>
+Testklasse für das Testen von higher/lower bei `BinSea`. Ausführbar über die main-Methode. Lässt sich einfach zu den anderen Klassen packen.
 Vor der Abgabe entfernen bzw. nicht mit abgeben, die Tests laufen sonst nicht durch.
 
-*Die Klasse testet auch mit int-Arrays, die mehrfach den selben Wert enthalten, das ist wohl nicht erforderlich.
+*Die Klasse testet auch mit `int`-Arrays, die mehrfach den selben Wert enthalten, das ist wohl nicht erforderlich.
 In dem Fall einfach die Testfälle, die mehrmals die 5 enthalten entfernen oder auskommentieren.*
 
-**Notiz: Bitte nicht mehr Version 1.0 oder älter verwenden! (aktuell: 1.2)**
+**Bitte nicht mehr Version 1.0 oder älter verwenden!**
 
-## [Blatt03](blatt03/)
-Momentan gibt es erstmal nur eine Testklasse für DynamicArray (was aber auch am wichtigsten ist), DynamicStack, StackyQueue und RingQueue. Lässt sich einfach zu den anderen Klassen packen und dann ausführen. Vor der Abgabe entfernen bzw. nicht mit abgeben, die Tests laufen sonst nicht durch.
+## [Blatt03](blatt03/) <span style="color:grey;font-size:large"> // Version 1.7</span>
+Momentan gibt es erstmal nur eine Testklasse für `DynamicArray` (was aber auch am wichtigsten ist), `DynamicStack`, `StackyQueue` und `RingQueue`. Lässt sich einfach zu den anderen Klassen packen und dann ausführen. Vor der Abgabe entfernen bzw. nicht mit abgeben, die Tests laufen sonst nicht durch.
 
 *Wichtig: es gibt auch noch weitere richtige Lösungen, die vielleicht nicht "optimal" sind. Meine Lösung geht davon aus, dass Elemente bei einer Größenänderung an den Arrayanfang geordnet werden und dass immer nur das nötigste kopiert wird (Kopieraufwand minimieren)*
 
-**Notiz: Bitte nicht mehr Version 1.0 oder älter verwenden! (aktuell: 1.7)**
+**Bitte nicht mehr Version 1.0 oder älter verwenden!**
 
 **Jetzt mit Tests zu DynamicStack, StackyQueue und RingQueue (ab Version 1.5)**
 
-## [Blatt05](blatt05/)
-Jetzt gibt es Tests zu Blatt 5 für die folgenden Klassen: DoubleHashInt, DoubleHashString, DoubleHashTable (sowohl mit Integer als auch String). Es gibt auch hilfreiche Diagramme in der Konsole, um die Qualität der Hashfunktionen zu bestimmen und Probleme besser zu erkennen. Lässt sich einfach zu den anderen Klassen packen und dann ausführen. Vor der Abgabe entfernen bzw. nicht mit abgeben, die Tests laufen sonst nicht durch.
+## [Blatt04](blatt04/) <span style="color:grey;font-size:large"> // Version 1.0</span>
+Sehr ähnlich zu den Tests für Blatt 5 gibt es jetzt auch welche für Blatt 4, diese Testen nur grob das Verhalten der `hash(String)`-Methode der Klasse `HashString`, das beinhaltet die Prüfung des Wertebereichs (0 bis m-1), der Konsistenz (mehrmaliges ausführen resultiert in den selben Hashwerten) und der Verteilung, auch mit Diagrammen. Lässt sich einfach zu den anderen Klassen packen und dann ausführen. Vor der Abgabe entfernen bzw. nicht mit abgeben, die Tests laufen sonst nicht durch.
 
-**Notiz: Bitte nicht mehr Version 1.0 oder älter verwenden!  (aktuell: 1.1)**
+```java
+// Diagramm Beispiel:
+Test 2: TEST STRING HASH m == 17
+  passed 2.1, result: hash() is consistent
+  passed 2.2, result min: 0 (>= 0)
+  passed 2.2, result max: 16 (<= 16)
+  passed 2.3, result: 51,500000 (expected: 50,000000, delta: 25,000000)
+ ┌───────────────────────────────────────────────────┐ (max: 60)
+ │                         █  █     █           █    │
+ │ █  ▄        █        ▄  █  █  █  █     █  █  █  █ │
+ │ █  █  ▄     █     █  █  █  █  █  █  ▄  █  █  █  █ │
+ │ █  █  █  █  █     █  █  █  █  █  █  █  █  █  █  █ │
+ │ █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │ █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │ █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │ █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │ █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │ █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ └─┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼─┘ (y-scale: 6,0)
+   ⁰  ¹  ²  ³  ⁴  ⁵  ⁶  ⁷  ⁸  ⁹ ¹⁰ ¹¹ ¹² ¹³ ¹⁴ ¹⁵ ¹⁶
+```
+
+## [Blatt05](blatt05/) <span style="color:grey;font-size:large"> // Version 1.1</span>
+Jetzt gibt es Tests zu Blatt 5 für die folgenden Klassen: `DoubleHashInt`, `DoubleHashString`, `DoubleHashTable` (sowohl mit Integer als auch String). Es gibt auch hilfreiche Diagramme in der Konsole, um die Qualität der Hashfunktionen zu bestimmen und Probleme besser zu erkennen. Lässt sich einfach zu den anderen Klassen packen und dann ausführen. Vor der Abgabe entfernen bzw. nicht mit abgeben, die Tests laufen sonst nicht durch.
+
+```java
+// Diagramm Beispiel:
+Test 2: TEST INT HASH_TICK
+  passed 2.1, result: hashTick() is consistent
+  passed 2.2, result min: 1 (>= 1)
+  passed 2.2, result max: 16 (<= 16)
+  passed 2.3, result: 53,000000 (expected: 53,125000, delta: 25,000000)
+ ┌───────────────────────────────────────────────────┐ (max: 54)
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ │    █  █  █  █  █  █  █  █  █  █  █  █  █  █  █  █ │
+ └─┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼─┘ (y-scale: 5,4)
+   ⁰  ¹  ²  ³  ⁴  ⁵  ⁶  ⁷  ⁸  ⁹ ¹⁰ ¹¹ ¹² ¹³ ¹⁴ ¹⁵ ¹⁶
+```
+
+**Bitte nicht mehr Version 1.0 oder älter verwenden!**
---
+++ blatt04/HashStringTest.java
@@ -0,0 +1,261 @@
+import static java.util.stream.Collectors.*;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.LongSummaryStatistics;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Random;
+import java.util.TreeMap;
+
+/**
+ * Öffentliche Testfälle von Christian Femers. Siehe
+ * https://github.com/MaisiKoleni/GAD-Extras
+ * 
+ * Bitte nicht mit abgeben. (geht sonst nicht)
+ * 
+ * @version 1.0
+ * 
+ * @author Christian Femers (IN.TUM)
+ *
+ */
+public class HashStringTest {
+
+	private static final String VERSION = "1.0";
+
+	private static Counter testNum = new Counter(0);
+	private static Counter testMethod = new Counter(0);
+
+	static {
+		tryVersionCheck();
+
+		System.out.println("CF's TESTS ACTIVE\n");
+	}
+
+	public static void main(String[] args) {
+		// Test 1
+		testString13();
+		// Test 2
+		testString17();
+	}
+
+	/**
+	 * Test String hashing m == 13
+	 */
+	public static void testString13() {
+		int m = 13;
+		int n = m * 50;
+		double dev = n / m / 2.0;
+		Random r = new Random(-2965331412945356690L);
+		Encoder enc64 = Base64.getEncoder().withoutPadding();
+
+		HashString hs = new HashString(m);
+		List<String> keys = r.ints(n).mapToObj(i -> enc64.encodeToString(BigInteger.valueOf(i).toByteArray()))
+				.collect(toList());
+
+		printTestHeadline("TEST STRING HASH m == 13"); // Test 1
+
+		var hMap = keys.stream().collect(groupingBy(hs::hash));
+
+		// 1 - checking consistency
+		assertEquals(keys.stream().collect(groupingBy(hs::hash)), hMap, "hash() %s consistent");
+		// 2 - checking bounds of the hash values
+		checkBounds(hMap.keySet(), 0, m - 1);
+		// 3 - checking collisions of hash
+		assertEquals(getMedian(hMap.values().stream().map(List::size).collect(toList())), n / m, dev);
+
+		// GRAPH
+		printDistribution(hMap, m, false);
+	}
+
+	/**
+	 * Test String hashing m == 17
+	 */
+	public static void testString17() {
+		int m = 17;
+		int n = m * 50;
+		double dev = n / m / 2.0;
+		Random r = new Random(-2990996836239250388L);
+		Encoder enc64 = Base64.getEncoder().withoutPadding();
+
+		HashString hs = new HashString(m);
+		List<String> keys = r.ints(n).mapToObj(i -> enc64.encodeToString(BigInteger.valueOf(i).toByteArray()))
+				.collect(toList());
+
+		printTestHeadline("TEST STRING HASH m == 17"); // Test 2
+
+		var hMap = keys.stream().collect(groupingBy(hs::hash));
+
+		// 1 - checking consistency
+		assertEquals(keys.stream().collect(groupingBy(hs::hash)), hMap, "hash() %s consistent");
+		// 2 - checking bounds of the hash values
+		checkBounds(hMap.keySet(), 0, m - 1);
+		// 3 - checking collisions of hash
+		assertEquals(getMedian(hMap.values().stream().map(List::size).collect(toList())), n / m, dev);
+
+		// GRAPH
+		printDistribution(hMap, m, false);
+	}
+
+	private static double getMedian(Collection<? extends Number> iCol) {
+		List<? extends Number> iList = new ArrayList<>(iCol);
+		Collections.sort(iList, (a, b) -> Double.compare(a.doubleValue(), b.doubleValue()));
+		return (iList.get(iList.size() / 2).doubleValue() + iList.get((iList.size() + 1) / 2).doubleValue()) / 2.0;
+	}
+
+	private static void checkBounds(Collection<? extends Number> nums, long min, long max) {
+		String testId = getNextTestId();
+		LongSummaryStatistics stats = nums.stream().mapToLong(Number::longValue).summaryStatistics();
+		if (stats.getMin() >= min)
+			System.out.format("  passed %s, result min: %d (>= %d)%n", testId, stats.getMin(), min);
+		else
+			System.err.format("  test %s failed min: %d (required: >= %d)%n", testId, stats.getMin(), min);
+		if (stats.getMax() <= max)
+			System.out.format("  passed %s, result max: %d (<= %d)%n", testId, stats.getMax(), max);
+		else
+			System.err.format("  test %s failed max: %d (required: <= %d)%n", testId, stats.getMax(), max);
+	}
+
+	/**
+	 * Simple assert-method to compare two objects.
+	 */
+	private static void assertEquals(Object actual, Object expected, String isMustBe) {
+		String testId = getNextTestId();
+		if (Objects.equals(actual, expected))
+			System.out.format("  passed %s, result: " + isMustBe + "%n", testId, "is");
+		else
+			System.err.format("  test %s failed: please note: " + isMustBe + "%n", testId, "must be");
+	}
+
+	/**
+	 * assert-method to compare two doubles with max difference delta.
+	 */
+	private static void assertEquals(double actual, double expected, double delta) {
+		String testId = getNextTestId();
+		if (Math.abs(actual - expected) <= delta)
+			System.out.format("  passed %s, result: %f (expected: %f, delta: %f)%n", testId, actual, expected, delta);
+		else
+			System.err.format("  test %s failed: expected %f but was %f (delta: %f)%n", testId, expected, actual,
+					delta);
+	}
+
+	private static String getNextTestId() {
+		return String.format("%s.%s", testMethod, testNum.incrementAndGet());
+	}
+
+	private static void printTestHeadline(String s) {
+		System.out.format("Test %s: %s%n", testMethod.incrementAndGet(), s);
+		testNum.reset();
+	}
+
+	private static void printDistribution(Map<Integer, ? extends List<?>> hMap, int size, boolean detail) {
+		var count = new TreeMap<>(hMap.entrySet().stream().collect(toMap(Entry::getKey, e -> e.getValue().size())));
+		long max = count.values().stream().mapToLong(i -> i).max().getAsLong();
+		double scale = max / 10.0;
+		if (scale < 1.0 || detail)
+			scale = 1.0;
+		int low = Math.min(count.firstKey().intValue(), 0);
+		int high = Math.max(count.lastKey().intValue(), size - 1);
+		String indent = " ";
+		System.out.print(indent + "\u250C");
+		for (int k = low; k <= high; k++) {
+			System.out.print("\u2500\u2500\u2500");
+		}
+		System.out.format("\u2510 (max: %d)%n", max);
+		for (double j = max - (scale / 2); j > 0; j -= scale) {
+			System.out.print(indent + "\u2502");
+			for (int k = low; k <= high; k++) {
+				long c = count.getOrDefault(k, 0);
+				System.out.print(c >= j ? " \u2588 " : (c > Math.max(j - scale / 2, 0) ? " \u2584 " : "   "));
+			}
+			System.out.println("\u2502");
+		}
+		System.out.print(indent + "\u2514");
+		for (int k = low; k <= high; k++) {
+			System.out.print("\u2500\u253C\u2500");
+		}
+		System.out.format("\u2518 (y-scale: %.1f)%n%s ", scale, indent);
+		for (int k = low; k <= high; k++) {
+			String num = superscript(k);
+			System.out.format(num.length() > 2 ? "%3s" : "%2s ", num);
+		}
+		System.out.println();
+	}
+
+	private static String superscript(long l) {
+		String old = String.valueOf(l);
+		StringBuilder res = new StringBuilder(old.length());
+		for (int i = 0; i < old.length(); i++) {
+			switch (old.charAt(i)) {
+			case '-':
+				res.append('\u207B');
+				break;
+			case '0':
+				res.append('\u2070');
+				break;
+			case '1':
+				res.append('\u00B9');
+				break;
+			case '2':
+				res.append('\u00B2');
+				break;
+			case '3':
+				res.append('\u00B3');
+				break;
+			default:
+				res.append((char) (old.charAt(i) + 0x2040));
+				break;
+			}
+		}
+		return res.toString();
+	}
+
+	/**
+	 * Counter that can count from 1 to 9 then A to Z (35 digits)
+	 */
+	public static class Counter {
+		private int count;
+
+		public Counter(int count) {
+			this.count = count;
+		}
+
+		public String incrementAndGet() {
+			count++;
+			return toString();
+		}
+
+		@Override
+		public String toString() {
+			return String.valueOf(toChar());
+		}
+
+		public char toChar() {
+			return (char) (count + (count < 10 ? 48 : 55));
+		}
+
+		public void reset() {
+			count = 0;
+		}
+	}
+
+	private static void tryVersionCheck() {
+		try {
+			Class.forName("CFUpdate").getDeclaredMethod("checkForNewVersion", String.class, Class.class, String.class)
+					.invoke(null, "blatt04", HashStringTest.class, VERSION);
+		} catch (@SuppressWarnings("unused") ClassNotFoundException e) {
+			System.out.println("Automatic update checks are inactive, download the CFUpdate class for that:");
+			System.out.println("https://raw.githubusercontent.com/MaisiKoleni/GAD-Extras/master/version/CFUpdate.java");
+			System.out.println();
+		} catch (IllegalArgumentException | ReflectiveOperationException | SecurityException e) {
+			System.err.println("Something went wrong invoking checkForNewerVersion:");
+			e.printStackTrace();
+		}
+	}
+}
